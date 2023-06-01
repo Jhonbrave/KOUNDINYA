45992c17db830e8f0ba4cf0b0765c8e3638d5ba0
Codder <122482673+Jhonbrave@users.noreply.github.com>
2023-06-01 12:31:53 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Jhonbrave/withouturl
+  git clone https://github.com/Jhonbrave/yash
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
