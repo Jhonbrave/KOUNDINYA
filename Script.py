66f67c608c977afaf14ff49cbefc11d410e50a73
Codder <122482673+Jhonbrave@users.noreply.github.com>
2023-06-01 10:31:44 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -17,15 +17,15 @@ class script(object):
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝙼𝚈 𝙷𝙴𝙻𝙿 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """<b><i>🤖 MY BOSS IS : <a href=https://t.me/kannada_hunters><b>Kannada Hunters</b></a>\n
-👨‍💻 ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href=https://t.me/kannada_hunters><b>Kannada Hunters</b></a>\n
-📝 SUPPORT : KANNADA HUNTERS\n
+👨‍💻 ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href=https://t.me/abcmovies_site><b>ABC MOVIES</b></a>\n
+📝 SUPPORT : KANNADA PIRATES \n
 📚 WHAT YOU GET : ALL MOVIES AND SERIES\n
-📡 ʜᴏsᴛᴇᴅ ᴏɴ : MARS\n
-📢 ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ : <a href=https://t.me/kannada_hunters><b></b>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a>\n
-🌟 ᴠᴇʀsɪᴏɴ : ᴠ 1.0\n</b></i>"""
+📡 SUBSCRIBE : KANNADA PIRATES\n
+📢 BOT UPDATES : <a href=https://t.me/abcmovies_site><b></b>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a>\n
+🌟 ᴠᴇʀsɪᴏɴ : ᴠ 1\n</b></i>"""
     SOURCE_TXT = """<b>SUPPORT:</b>
 » US<b>
-» EXPLORE ON @kannada_hunters<b>"""
+» EXPLORE ON @abcmovies_site<b>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
 - Filter is the feature were users can set automated replies for a particular keyword and Search Bot will respond whenever a keyword is found the message
